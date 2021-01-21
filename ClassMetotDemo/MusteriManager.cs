f84d1dffa3405a628aaadaa895b740c1acf17630
Beyza Baltacı <beyza_baltacii@hotmail.com>
2021-01-21 19:25:55 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Sayın" +" "+ musteri.Id +" " + musteri.Adi + " "+ musteri.Soyadi +" " + "kaydınız tamamlanmıştır.");
+        }
+        public void Guncelleme(Musteri musteri)
+        {
+            Console.WriteLine("Sayın" + " " + musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + "güncellemeniz tamamlanmıştır.");
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Sayın" + " " + musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + "isimli kaydın silme işlemi tamamlanmıştır.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 123;
+            musteri1.Adi = "Beyza";
+            musteri1.Soyadi = "Baltacı";
+            musteri1.Sehir = "İstanbul";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1234;
+            musteri2.Adi = "Belgin";
+            musteri2.Soyadi = "Kaya";
+            musteri2.Sehir = "Ankara";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Guncelleme(musteri1);
+            musteriManager.Guncelleme(musteri2);
+            musteriManager.Silme(musteri1);
+            musteriManager.Silme(musteri2);
+
+
+        }
+    }
+}
